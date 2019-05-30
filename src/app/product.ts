62dd437da9c7e8a1c28d0902af4c5b8f1965b421
AGEBW <a.d.river@hotmail.com>
2019-05-30 11:19:01 -0600

    Por fin salio la version correcta de Productos por AGEBW

---
+++ src/app/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    // tslint:disable-next-line:variable-name
+    _id: string;
+    // tslint:disable-next-line:variable-name
+    prod_name: string;
+    // tslint:disable-next-line:variable-name
+    prod_desc: string;
+    // tslint:disable-next-line:variable-name
+    prod_price: number;
+    // tslint:disable-next-line:variable-name
+    updated_at: Date;
+    //prod_img :string;
+  }
